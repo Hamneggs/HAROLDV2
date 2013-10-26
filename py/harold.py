3c936e87fe8da0392e9a48b2c6cf688497a17d3c
Gerard Geer <gdg1546@rit.edu>
2013-10-26 01:36:36 -0700

    Fleshed out the Python naughty bits of Harold a bit more. We now query a hypothetical redis database for a user's songs, split it using PyDub, and play the split song using PyAudio. Hypothetically; the database doesn't exist and the modules aren't yet installed.

--- py/harold.py
+++ py/harold.py
@@ -1,9 +1,11 @@
 # Import dependencies.
 print "Loading dependencies..."
 import pydub
+import pyaudio
+import redis
 import serial
+import cshldap
 import os
-#import cshldap
 
 def loadSong(filename):
 	"""
@@ -11,13 +13,13 @@ def loadSong(filename):
 	file's type matches its extension.
 
 	Prerequisites:
-		File type must match extension.
+		-File type must match extension.
 
 	Parameters:
-		filename (String):	The full name of the file, with a local path.
+		-filename (String):	The full name of the file, with a local path.
 
 	Returns:
-		A Pydub Audio Segment filled with the requested song.
+		-A Pydub Audio Segment filled with the requested song.
 	"""
 	# Split off the file extension from the file name.
 	name, extension = os.path.splitext(filename)
@@ -31,10 +33,10 @@ def splitSong(songSegment, start, finish):
 	Splits an AudioSegment, making sure that the start and finish bounds are safe.
 
 	Prerequisites:
-		All passed parameters are valid.
+		-All passed parameters are valid.
 
 	Parameters:
-		songSegment (AudioSegment):	The Pydub AudioSegment that contains the song.
+		-songSegment (AudioSegment):	The PyDub AudioSegment that contains the song.
 
 	Returns:
 		The song, split properly.
@@ -55,51 +57,131 @@ def splitSong(songSegment, start, finish):
 	else:
 		return songSegment[start*1000:finish*1000]
 	
+def playSong(songSegment):
+	"""
+	Plays a PyDub AudioSegment.
+	
+	Prerequisites:
+		-The given AudioSegment has already been loaded and split.
+	
+	Parameters:
+		-songSegment (AudioSegment): 	The PyDub AudioSegement that contains the
+										audio to be played.
 		
+	Returns:
+		-None
+	"""
+	# Store the number of frames to write to the stream each time.
+	framesPer = 1024;
 	
-
-# connect to LDAP.
-# construct dictionary of iButton ids to user names.
-# disconnect from LDAP.
-
-# Create serial connection.
-print "Creating serial connection..."
-ser = serial.Serial( port='/dev/ttyUSB0', baudrate=4800 )
-
-# Open and verify serial port.
-print "Opening serial connection..."
-ser.open()
-
-if( ser.isOpen() ):
-	print "Serial port is open and ready for use."
-else:
-	print "Serial port was not opened correctly."
-	input("Press any key to exit.")
-	exit()
-
-# Clear existing data.
-print "Flushing pre-existing serial data."
-ser.flushInput()
-
-# Enter serial waiting loop.
-while True:
-
-	# read serial number.
-	id = ser.readline()
-	id = id[1:] # trim first character.
-	id = id.rstrip() # remove leading and trailing white space.
+	# Open a stream with our PyAudio instance to play the song selection.
+	stream = audio.open(format=p.get_format_from_width(songSegment.sample_width),
+						channels=songSegment.channels,
+						rate=songSegment.frame_rate,
+						output=True)
 	
-	# plug it in to the dictionary so that we can the the user name.
+	# You know this shit is sweet.
+	for frame in range(0, len(songSegment), framesPer):
+		stream.write( songSegment[frame:(frame+framesPer)] );
+		
+def getSongRandomly(userID):
+	"""
+	Opens a connection to the local Redis server, and gets a random song
+	selection.
 	
-	# open mySql connection.
-	# get list of current songs and playtimes.
-	# close mySql connection.
+	Prerequisites:
+		-UserID is properly formed.
 	
-	# pick a song from the selection of current songs.
+	Parameters:
+		-userID (String): 	The ID of the current user, used in querying the 
+							Redis database.
 	
-	# load the song.
-	song = loadSong("placeholder.wav")
+	Returns:
+		-A dictionary containing the various properties of a random song
+		that the user has saved as selected. The keys of the dictionary are:
+			-"name":	The name of the song. This is stored mainly for the interface.
+			-"path":	The file path to where the file is stored.
+			-"start": 	The starting point within the song.
+			-"stop":	The stopping point within the song.
+			-"volume":	The volume at which to play the song.
+	"""
+	# Open up a Redis connection.
+	r = redis.StrictRedis(host='localhost', port=6379, db='Harold')
 	
-	# get the timing parameters from the song listing.
-	# play it, using the parameters associated with the song.
-	# Clear serial input so stacking doesn't occur.
+	# Get a random song from the user's selection.. Note that songs are
+	# stored independent of users. Each userID key points to a 'set' of
+	# keys that point to songs, which are hashes.
+	songFromSet = r.SRANDMEMBER(userID, 1);
+	
+	# Now that we have a key to a song has, we get all the data from it
+	# return it.
+	songProperties = r.HGETALL(songFromSet);
+	
+	# If we can't get a song from the user, we just return a dictionary
+	# full of the place holder song's properties.
+	if songProperties == None:
+		return {"name": 'placeholderSong.wav', "path": 'usr/Harold/songs/placeholderSong.wav', "start":0, "stop":20, "volume":1}
+	else return songProperties;
+	
+def main():
+	"""
+	The main execution of Harold's Python naughty-bits. 
+	"""
+	# connect to LDAP.
+	# construct dictionary of iButton ids to user names.
+	# disconnect from LDAP.
+
+	# Create PyAudio instance, and a stream to pump sound through.
+	audio = pyaudio.PyAudio();
+	
+	# Create serial connection.
+	print "Creating serial connection..."
+	ser = serial.Serial( port='/dev/ttyUSB0', baudrate=4800 )
+
+	# Open and verify serial port.
+	print "Opening serial connection..."
+	ser.open()
+
+	if( ser.isOpen() ):
+		print "Serial port is open and ready for use."
+	else:
+		print "Serial port was not opened correctly."
+		input("Press any key to exit.")
+		exit()
+
+	# Clear existing data.
+	print "Flushing pre-existing serial data."
+	ser.flushInput()
+
+	# Enter serial waiting loop.
+	while True:
+
+		# read serial number.
+		id = ser.readline()
+		id = id[1:] # trim first character.
+		id = id.rstrip() # remove leading and trailing white space.
+		
+		# plug it in to the dictionary so that we can the the user name.
+		
+		# pick a song listing from the selection of current songs.
+		print "Selecting song..."
+		selection = getSongRandomly(id)
+		print "Song: "+selection["name"]+"( "+selection["path"]+" )"
+		
+		# load the song.
+		print "Loading song data..." 
+		song = loadSong(selection["path"])
+		
+		# Trim the song.
+		print "Trimming song..."
+		splitSong(song, selection["start"], selection["stop"])
+		
+		# Play the pruned song.
+		print "Playing song..."
+		playSong(song);
+		
+		# Clear serial input so stacking doesn't occur.
+		ser.flushInput();
+		
+# Execute that shit!
+main()
